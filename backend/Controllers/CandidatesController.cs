c48ac598a1ce125d114e8244035e6945580d2dd4
EmiPeralta <emilianop96@hotmail.com>
2020-11-15 20:08:09 -0300

    Backend finished for now

---
+++ backend/Controllers/CandidatesController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using backend.Models;
+
+namespace backend.Controllers
+{
+    [Route("api/candidates")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        private readonly DonationContext _context;
+
+        public CandidatesController(DonationContext context)
+        {
+            _context = context; //dependency injection
+        }
+
+        
+        [HttpGet] // GET: api/candidates
+        public async Task<ActionResult<IEnumerable<Candidate>>> GetCandidates()
+        {
+            return await _context.Candidates.ToListAsync();
+        }
+
+        
+        [HttpGet("{id}")] // GET: api/candidates/5
+        public async Task<ActionResult<Candidate>> GetCandidate(int id)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            return candidate;
+        }
+
+        
+        [HttpPut("{id}")] // PUT: api/candidates/5
+        public async Task<IActionResult> PutCandidate(int id, Candidate candidate)
+        {
+            if (id != candidate.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(candidate).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CandidateExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        
+        [HttpPost] // POST: api/candidates
+        public async Task<ActionResult<Candidate>> PostCandidate(Candidate candidate)
+        {
+            _context.Candidates.Add(candidate);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCandidate", new { id = candidate.Id }, candidate);
+        }
+
+        
+        [HttpDelete("{id}")] // DELETE: api/candidates/5
+        public async Task<ActionResult<Candidate>> DeleteCandidate(int id)
+        {
+            var candidate = await _context.Candidates.FindAsync(id);
+            if (candidate == null)
+            {
+                return NotFound();
+            }
+
+            _context.Candidates.Remove(candidate);
+            await _context.SaveChangesAsync();
+
+            return candidate;
+        }
+         
+        private bool CandidateExists(int id)
+        {
+            return _context.Candidates.Any(e => e.Id == id);
+        }
+    }
+}
